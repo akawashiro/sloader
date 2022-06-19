9b01addb4c4f84f66c1dd776431825cd50303faa
Akira Kawata <akirakawata1@gmail.com>
2022-06-19 16:50:09 +0900

    Update build_glibc.sh

--- build_glibc.sh
+++ build_glibc.sh
@@ -1,5 +1,6 @@
 #! /bin/bash -eux
 
 cd $(git rev-parse --show-toplevel)/glibc-build
-LD_LIBRARY_PATH="" make all -j $(($(nproc) - 1))
-LD_LIBRARY_PATH="" make install -j $(($(nproc) - 1))
+LD_LIBRARY_PATH="" make clean
+LD_LIBRARY_PATH="" make all -j 20
+LD_LIBRARY_PATH="" make install -j 20

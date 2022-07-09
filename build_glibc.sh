20acdccd0698d3afc5dee848598975ebab946277
Akira Kawata <akirakawata1@gmail.com>
2022-07-09 20:56:16 +0900

    Use baar to generate compile_commands.json

--- build_glibc.sh
+++ build_glibc.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -eux
 
 cd $(git rev-parse --show-toplevel)/glibc-build
-LD_LIBRARY_PATH="" make all -j 20
-LD_LIBRARY_PATH="" make install -j 20
+LD_LIBRARY_PATH="" bear -- make all -j 20
+LD_LIBRARY_PATH="" bear -- make install -j 20

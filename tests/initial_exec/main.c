103284de4fbb4789a91cfe21be0f258b62061269
Akira Kawata <akirakawata1@gmail.com>
2022-08-03 22:31:19 +0900

    WIP

--- tests/initial_exec/main.c
+++ tests/initial_exec/main.c
@@ -3,18 +3,45 @@
 
 #include "/home/akira/sloader/raw_write.h"
 
-extern __thread uint32_t i;
+extern __thread uint32_t i0;
+extern __thread uint32_t i1;
+extern __thread uint32_t i2;
+extern __thread uint32_t i3;
 extern uint32_t j;
 
 int main() {
-    RAW_PRINT_STR("__thread uint32_t i=");
-    RAW_PRINT_HEX(i);
-    RAW_PRINT_STR("\n&i=");
-    RAW_PRINT_HEX(&i);
+    RAW_PRINT_STR("__thread uint32_t i0=");
+    RAW_PRINT_HEX(i0);
+    RAW_PRINT_STR("\n&i0=");
+    RAW_PRINT_HEX(&i0);
+    RAW_PRINT_STR("\n");
+
+    RAW_PRINT_STR("__thread uint32_t i1=");
+    RAW_PRINT_HEX(i1);
+    RAW_PRINT_STR("\n&i1=");
+    RAW_PRINT_HEX(&i1);
+    RAW_PRINT_STR("\n");
+
+    RAW_PRINT_STR("__thread uint32_t i2=");
+    RAW_PRINT_HEX(i2);
+    RAW_PRINT_STR("\n&i2=");
+    RAW_PRINT_HEX(&i2);
+    RAW_PRINT_STR("\n");
+
+    RAW_PRINT_STR("__thread uint32_t i3=");
+    RAW_PRINT_HEX(i3);
+    RAW_PRINT_STR("\n&i3=");
+    RAW_PRINT_HEX(&i3);
+    RAW_PRINT_STR("\n");
+
+    RAW_BREAK();
+    i0 = 0xeeeeeeee;
+    i1 = 0xffffffff;
 
     RAW_PRINT_STR("\nShould be 0xaabbccdd: uint32_t j=");
     RAW_PRINT_HEX(j);
     RAW_PRINT_STR("\n&j=");
     RAW_PRINT_HEX(&j);
+    RAW_PRINT_STR("\n");
     return 0;
 }

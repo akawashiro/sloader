35adda6b7eab1fff3563f2f83b3ea57852753096
Akira Kawata <akirakawata1@gmail.com>
2022-08-08 19:30:07 +0900

    Add initial_exec test

--- tests/initial_exec/hoge.c
+++ tests/initial_exec/hoge.c
@@ -1,4 +1,7 @@
 #include <stdint.h>
 
-__thread uint32_t i = 0xdeadbeef;
+__thread uint32_t i0 = 0xaaaaaaaa;
+__thread uint32_t i1 = 0xbbbbbbbb;
+__thread uint32_t i2 = 0xcccccccc;
+__thread uint32_t i3 = 0xdddddddd;
 uint32_t j = 0xaabbccdd;

2c56b29ef3cfb59d482ce091ed8d3ae88ec9b728
Akira Kawata <akirakawata1@gmail.com>
2022-11-20 21:58:42 +0900

    Fix path problem

--- tests/dlopen/main.c
+++ tests/dlopen/main.c
@@ -1,7 +1,13 @@
 #include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    void* handle = dlopen("libdlopen_hoge.so", RTLD_NOW);
+    void* handle = dlopen("./libdlopen_hoge.so", RTLD_NOW);
+    if(handle == NULL){
+        printf("handle is NULL\n");
+        exit(1);
+    }
     void (*hoge_func)();
     *(void**)(&hoge_func) = dlsym(handle, "hoge");
     hoge_func();
--- tests/dlopen2/main.c
+++ tests/dlopen2/main.c
@@ -1,13 +1,17 @@
 #include <dlfcn.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void fromMain(){
     printf("From main\n");
 }
 
 int main(){
-    void* handle = dlopen("libdlopen2_hoge.so", RTLD_NOW);
-    if(handle == NULL) return 1;
+    void* handle = dlopen("./libdlopen2_hoge.so", RTLD_NOW);
+    if(handle == NULL){
+        printf("handle is NULL\n");
+        exit(1);
+    }
     void (*hoge_func)();
     *(void**)(&hoge_func) = dlsym(handle, "hoge");
     hoge_func();

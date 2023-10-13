d018fad9a2e38ed99475ebcb07912a9431367fd5
Akira Kawata <akirakawata1@gmail.com>
2023-10-14 00:14:22 +0900

    Use custom libc for debian

--- scripts/clone_glibc.sh
+++ scripts/clone_glibc.sh
@@ -2,14 +2,21 @@
 
 cd $(git rev-parse --show-toplevel)
 
+if [[ $# -ne 1 ]]
+then
+    echo "Usage: $0 <glibc-version>"
+    exit 1
+fi
+GLIBC_VERSION=$1
+
 if [[ ! -d glibc ]]
 then
-    git clone git@github.com:akawashiro/glibc.git
+    git clone --depth 1 --no-single-branch https://github.com/akawashiro/glibc.git
     
     pushd glibc
     git remote add upstream https://sourceware.org/git/glibc.git
     git fetch --all
-    git checkout glibc-2.35
+    git checkout ${GLIBC_VERSION}
     popd
 fi
 
